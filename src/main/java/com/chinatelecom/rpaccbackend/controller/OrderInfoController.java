23eabe8240471167042b4371f5929ca944e6e871
ctlubz <1013556811@qq.com>
2022-11-12 11:18:24 +0800

    OrderPool更改状态筛选，添加CrossOrigin

--- src/main/java/com/chinatelecom/rpaccbackend/controller/OrderInfoController.java
+++ src/main/java/com/chinatelecom/rpaccbackend/controller/OrderInfoController.java
@@ -4,6 +4,7 @@ import com.chinatelecom.rpaccbackend.pojo.OrderInfo;
 import com.chinatelecom.rpaccbackend.pojo.common.Result;
 import com.chinatelecom.rpaccbackend.service.OrderInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/orderinfo")
+@CrossOrigin
 public class OrderInfoController {
     @Autowired
     private OrderInfoService orderInfoService;
--- src/main/java/com/chinatelecom/rpaccbackend/controller/OrderPoolController.java
+++ src/main/java/com/chinatelecom/rpaccbackend/controller/OrderPoolController.java
@@ -1,9 +1,12 @@
 package com.chinatelecom.rpaccbackend.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.chinatelecom.rpaccbackend.pojo.OrderPool;
 import com.chinatelecom.rpaccbackend.pojo.common.Result;
 import com.chinatelecom.rpaccbackend.service.OrderPoolService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,11 +14,14 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/orderpool")
+@CrossOrigin
 public class OrderPoolController {
     @Autowired
     private OrderPoolService orderPoolService;
     @GetMapping("index")
     public Result<List<OrderPool>> orderPoolIndex(){
+        LambdaQueryWrapper<OrderPool> lambdaQueryWrapper = new LambdaQueryWrapper<>();
+        lambdaQueryWrapper.eq(OrderPool::getOrderStatus, 0);
         return Result.ok(orderPoolService.orderPoolIndex());
     }
 }
