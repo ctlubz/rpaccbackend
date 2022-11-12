2d2621b3e0dd8ada1a79d668a2e8b48d4c831c67
ctlubz <1013556811@qq.com>
2022-11-12 10:06:42 +0800

    编写OrderInfo,OrderPool DAO层

---
+++ src/main/java/com/chinatelecom/rpaccbackend/dao/OrderPoolDAO.java
@@ -0,0 +1,9 @@
+package com.chinatelecom.rpaccbackend.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.chinatelecom.rpaccbackend.pojo.OrderPool;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface OrderPoolDAO extends BaseMapper<OrderPool> {
+}
