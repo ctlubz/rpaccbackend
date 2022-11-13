14c3197ab7b74d6c1564111ad76a4e17c24fe861
ctlubz <1013556811@qq.com>
2022-11-13 14:48:59 +0800

    添加OrderHistoryDAO层，修改OrderHistory实体类

--- src/main/java/com/chinatelecom/rpaccbackend/pojo/OrderHistory.java
+++ src/main/java/com/chinatelecom/rpaccbackend/pojo/OrderHistory.java
@@ -1,9 +1,11 @@
 package com.chinatelecom.rpaccbackend.pojo;
 
+import com.alibaba.fastjson.JSON;
 import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.extension.handlers.FastjsonTypeHandler;
 import lombok.Data;
 
 import java.util.Date;
@@ -15,8 +17,8 @@ public class OrderHistory {
     private Long orderId;
     @TableField("busi_type")
     private String busiType;
-    @TableField("remark")
-    private String remark;
+    @TableField(value = "remark", typeHandler = FastjsonTypeHandler.class)
+    private JSON remark;
     @TableField("order_status")
     private Integer orderStatus;
     @TableField("acceptor_id")
