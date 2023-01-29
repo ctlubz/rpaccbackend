fe0e30ff882e63a589094f279efefc86030f70d9
ctlubz <1013556811@qq.com>
2023-01-29 17:18:39 +0800

    清理多余代码

--- src/main/java/com/chinatelecom/rpaccbackend/controller/OrderInfoController.java
+++ src/main/java/com/chinatelecom/rpaccbackend/controller/OrderInfoController.java
@@ -19,54 +19,16 @@ import java.util.Objects;
 @CrossOrigin
 @Api(tags = "工单信息")
 public class OrderInfoController {
-    @Autowired
-    private OrderInfoService orderInfoService;
-    @Autowired
-    private OrderPoolService orderPoolService;
-    @GetMapping("index")
-    @ApiOperation("查询所有")
-    public Result<List<OrderInfo>> orderInfoIndex(){
-        return Result.ok(orderInfoService.orderInfoIndex());
+    private final OrderInfoService orderInfoService;
+
+    public OrderInfoController(OrderInfoService orderInfoService) {
+        this.orderInfoService = orderInfoService;
     }
+
     @PutMapping("update")
     @ApiOperation("更新工单备注")
     public Result<Object> updateRemark(@RequestBody UpdateRemarkVO updateRemarkVO) throws Exception{
         orderInfoService.updateRemarkById(updateRemarkVO);
         return Result.ok().message("更新备注成功");
-    }
-    @GetMapping("prodshutdown")
-    @ApiOperation("机器人产品停机取数")
-    public Result<Object> shutdownInfo() throws Exception {
-        return Result.ok(orderInfoService.shutdownInfo());
-    }
-    @PostMapping("add")
-    @ApiOperation("添加订单信息接口")
-    public Result<Object> orderInfoInsert(@RequestBody OrderInfoVO orderInfoVO){
-        //判断工单号是否为空
-        if (Objects.isNull(orderInfoVO.getOrderId())){
-            return Result.fail().message("工单号不能为空");
-        }
-        //检索工单是否已经存在
-        OrderInfo orderInfo = orderInfoService.selectOneById(orderInfoVO.getOrderId());
-        if(!Objects.isNull(orderInfo)){
-            return Result.fail().message("工单已存在");
-        }
-//        System.out.println(Objects.isNull(orderInfo));
-//        System.out.println(orderInfoVO);
-        //插入OrderInfo
-        orderInfo = new OrderInfo();
-        orderInfo.setOrderId(orderInfoVO.getOrderId());
-        orderInfo.setInitTime(orderInfoVO.getInitTime());
-        orderInfo.setCustomerNumber(orderInfoVO.getCustomerNumber());
-        orderInfo.setRemark(orderInfoVO.getRemark());
-        if(!Objects.isNull(orderInfoVO.getLocalNet())){
-            orderInfo.setLocalNet(orderInfoVO.getLocalNet());
-        }
-        Integer result = orderInfoService.insertOrderInfo(orderInfo);
-        if(result != 0){
-            orderPoolService.insertOrderPool(orderInfo.getOrderId(), orderInfo.getRemark(), orderInfo.getBusiType());
-            return Result.ok(result);
-        }
-        return Result.fail().message("请求失败");
     }
 }
--- src/main/java/com/chinatelecom/rpaccbackend/service/OrderInfoService.java
+++ src/main/java/com/chinatelecom/rpaccbackend/service/OrderInfoService.java
@@ -26,10 +26,6 @@ public class OrderInfoService {
     @Autowired
     private OrderPoolDAO orderPoolDAO;
 
-    public List<OrderInfo> orderInfoIndex(){
-        return orderInfoDAO.selectList(null);
-    }
-
     public OrderInfo selectOneById(Long id){
         return orderInfoDAO.selectById(id);
     }
@@ -47,41 +43,5 @@ public class OrderInfoService {
         orderPool.setOrderStatus(OrderStatusEnum.WAITING.getCode());
         orderPoolDAO.updateById(orderPool);
         // @TODO 4. 打给机器人
-    }
-    public Integer insertOrderInfo(OrderInfo orderInfo){
-        return orderInfoDAO.insert(orderInfo);
-    }
-    public JSON shutdownInfo() throws Exception {
-        // 1. 从OrderPool中选出状态为3的
-        LambdaQueryWrapper<OrderPool> lambdaQueryWrapper = new LambdaQueryWrapper<>();
-        lambdaQueryWrapper.eq(OrderPool::getOrderStatus, OrderStatusEnum.WAITING.getCode()).last("limit 1");
-        OrderPool orderPool = orderPoolDAO.selectOne(lambdaQueryWrapper);
-        // 如果找不到
-        if(Objects.isNull(orderPool)){
-            return null;
-        }
-        // 2. 从OrderInfo中选出信息
-        OrderInfo orderInfo = orderInfoDAO.selectById(orderPool.getOrderId());
-        Matcher matcher = StringSplit.numberPattern.matcher(orderInfo.getRemark());
-        // 3. 往数据中添加电话号码等
-        String result = orderPool.getRemark();
-        JSONObject jsonObject = JSONObject.parseObject(result);
-        if(matcher.find()){
-            jsonObject.put("业务号码", matcher.group());
-        }
-        else{
-            throw new Exception("没有业务号码");
-        }
-        jsonObject.put("工单号", orderInfo.getOrderId());
-        //如果本地网为空
-        if(Objects.isNull(orderInfo.getLocalNet())){
-            jsonObject.put("归属本地网", "铜川本地网");
-        }
-        else {
-            jsonObject.put("归属本地网", orderInfo.getLocalNet());
-        }
-            jsonObject.put("业务类型", "套餐停机");
-        // @TODO 4. 更改订单状态为执行中
-        return jsonObject;
     }
 }
